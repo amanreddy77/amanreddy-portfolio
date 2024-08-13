bc5a0518a3c118738d76dec19bab8a3a7a32e0d0
Aman Reddy p <amanreddyp@Amans-MacBook-Air-2.local>
2024-08-14 04:25:06 +0530

    last change

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./layout/Home";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
       </Routes>
-      <SpeedInsights/>
+      <SpeedInsights />
+      <Analytics/>
     </>
   );
 }
